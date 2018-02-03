2a5fa61337c26f5dbe4a2fc87ae99b3f6c5a6ddd
Steve Conover <sconover@gmail.com>
2018-02-03 09:51:08 +0100

    fullapp

--- make_full_app_bundle.sh
+++ make_full_app_bundle.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 
-NODE_PATH=nodedeps/node_modules node nodedeps/node_modules/browserify/bin/cmd.js src/init.js -o out/app.js
+NODE_PATH=nodedeps/node_modules node nodedeps/node_modules/browserify/bin/cmd.js src/init.js -o out/fullapp.js
