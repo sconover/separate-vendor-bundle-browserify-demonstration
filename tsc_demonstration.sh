22b4372364f507bd2aaabb4b52015646de925f3a
Steve Conover <sconover@gmail.com>
2018-02-03 11:41:42 +0100

    --diagnostics and --skipLibCheck - the latter results in a significant speedup in tsc

--- tsc_demonstration.sh
+++ tsc_demonstration.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -ex
 
-nodedeps/node_modules/.bin/tsc -p . --outDir tscout
+# re: --diagnostics, see https://stackoverflow.com/questions/36624273/investigating-long-typescript-compile-times
+
+# re: --skipLibCheck, see: https://github.com/Microsoft/TypeScript/issues/13538#issuecomment-273327029
+# "Check time" seems to have a floor of about 500ms without this. With this flag, it's basically zero.
+
+time nodedeps/node_modules/.bin/tsc -p . --outDir tscout --diagnostics --skipLibCheck
--- tscout/init.js
+++ tscout/init.js
@@ -1,5 +1,5 @@
 "use strict";
 exports.__esModule = true;
-var long_1 = require("long");
-var longVal = new long_1["default"](0xFFFFFFFF, 0x7FFFFFFF);
+var Long = require("long");
+var longVal = new Long(0xFFFFFFFF, 0x7FFFFFFF);
 console.log(longVal.toString());
