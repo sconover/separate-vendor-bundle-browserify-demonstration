045b6cc74eca34ebe47b13b3ca09942aa70f6dee
Steve Conover <sconover@gmail.com>
2018-02-03 11:21:12 +0100

    ts make full app bundle

--- make_vendor_bundle.sh
+++ make_vendor_bundle.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 
-NODE_PATH=nodedeps/node_modules node nodedeps/node_modules/browserify/bin/cmd.js -r long -o out/vendor.js
+NODE_PATH=nodedeps/node_modules time node nodedeps/node_modules/browserify/bin/cmd.js -r long -o out/vendor.js
--- out/tsfullapp.js
+++ out/tsfullapp.js
@@ -1212,8 +1212,8 @@
 },{}],2:[function(require,module,exports){
 "use strict";
 exports.__esModule = true;
-var long_1 = require("long");
-var longVal = new long_1["default"](0xFFFFFFFF, 0x7FFFFFFF);
+var Long = require("long");
+var longVal = new Long(0xFFFFFFFF, 0x7FFFFFFF);
 console.log(longVal.toString());
 
 },{"long":1}]},{},[2]);
